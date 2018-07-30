e9b2b90c829ab803193032281a1af8c72d775c3c
Denis Yakupov <den-yakupovr@yandex.ru>
2018-07-30 13:40:19 +0300

    Initial commit

---
+++ b916873x.beget.tech/public_html/config/params.php
@@ -0,0 +1,9 @@
+<?php
+
+    return [
+        'admin_email' => 'den@mail.com',
+        'shop_name' => 'Тестовый магазин',
+        'pagination' => '3',
+        'smtp_login' => '',
+        'smtp_password' => ''
+    ];
